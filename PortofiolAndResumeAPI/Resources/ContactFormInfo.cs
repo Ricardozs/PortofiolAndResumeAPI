eb221f251b50dfad61fa4da6368223290216be21
Ricardo Sanchez <ricardo.sanchez@vhb.com>
2020-06-08 07:56:06 -0500

    email implementation

---
+++ PortofiolAndResumeAPI/Resources/ContactFormInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortofiolAndResumeAPI.Resources
+{
+    public class ContactFormInfo
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string request { get; set; }
+        public string message { get; set; }
+        public string company { get; set; }
+    }
+}
---
+++ PortofiolAndResumeAPI/Services/ContactService.cs
@@ -0,0 +1,40 @@
+﻿using PortofiolAndResumeAPI.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace PortofiolAndResumeAPI.Services
+{
+    public class ContactService
+    {
+        protected const string myEmail = "ricardo-ss@hotmail.com";
+        protected const string password = "Viviana12";
+        public async Task<bool> SendEmail(ContactFormInfo info)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress(info.email);
+                message.To.Add(new MailAddress(myEmail));
+                message.Subject = info.request;
+                message.Body = info.message;
+                smtp.Port = 587;
+                smtp.Host = "smtp.live.com";
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(myEmail, password);
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+                return await Task.Run(() => true);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
